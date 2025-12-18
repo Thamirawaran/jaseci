9ac6107799ccfd631ea4efa9a25917e029ef50b3
Thamirawaran <107134124+Thamirawaran@users.noreply.github.com>
2025-12-18 13:02:19 +0530

    support proprs as argument

--- jac/tests/compiler/passes/ecmascript/test_js_generation.py
+++ jac/tests/compiler/passes/ecmascript/test_js_generation.py
@@ -176,6 +176,13 @@ def test_advanced_fixture_emits_expected_constructs(
         in js_code
     )
 
+    assert "function PropTodoList(props) {" in js_code
+    assert "const {filteredTodos, toggleTodo, deleteTodo} = props;" in js_code
+    assert (
+        '"toggleTodo": props.toggleTodo, "deleteTodo": props.deleteTodo, "children": props.children}, []);'
+        in js_code
+    )
+
     assert_balanced_syntax(js_code, advanced_fixture)
     assert_no_jac_keywords(js_code, advanced_fixture)
     assert len(js_code) > 150
