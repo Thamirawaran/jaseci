5dc6595b220b19a46bf3fc01c29d238dd8da7da1
Thamirawaran <107134124+Thamirawaran@users.noreply.github.com>
2025-12-18 13:28:54 +0530

    props as keyword

--- jac/jaclang/pycore/jac_parser.py
+++ jac/jaclang/pycore/jac_parser.py
@@ -890,6 +890,7 @@ class JacParser(Transform[uni.Source, uni.Module]):
                         | KW_ROOT
                         | KW_SUPER
                         | KW_SELF
+                        | KW_PROPS
                         | KW_HERE
                         | KW_VISITOR
             """
@@ -3698,6 +3699,7 @@ class JacParser(Transform[uni.Source, uni.Module]):
                 Tok.KW_ROOT,
                 Tok.KW_SUPER,
                 Tok.KW_SELF,
+                Tok.KW_PROPS,
                 Tok.KW_HERE,
                 Tok.KW_VISITOR,
             ]:
